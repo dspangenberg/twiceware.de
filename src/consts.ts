de9f8c8d88c1273cb9c83439954dbf0fc31982ce
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-01-15 22:37:52 +0100

    ...

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "twiceware.de",
+  DESCRIPTION: "twiceware solutions e. K. in Bonn - Webentwicklung seit 2004",
+  AUTHOR: "Danny Spangenberg",
 }
 
 // Work Page
